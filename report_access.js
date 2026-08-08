bf7d454a78521bf6a7f5664cc62ccceef9fb8d9f
Kerem <kerem@local.dev>
2026-08-08 10:28:10 +0300

    Fix report portal access and update market cockpit

--- report_access.js
+++ report_access.js
@@ -1,4 +1,4 @@
-﻿(function () {
+(function () {
   const storageKey = "report_access_2026_07_13_v1";
   const allowed = [
     "320f8da18c9bff2b3776f9aa2bc72eaee31fccd05b7683d6929a7ca8a916c586"
@@ -8,4 +8,3 @@
     window.location.replace("index.html");
   }
 })();
-
